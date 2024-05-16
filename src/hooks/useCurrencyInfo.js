b840f6b853282cff7bc4e5dae7eb32a41188bc02
Padmalochan Barik <padmalochanbarik98@gmail.com>
2024-05-16 16:31:26 +0530

    first commit

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency]);
+  return data;
+}
+
+export default useCurrencyInfo;
